87480fb8cff0d294002889c3c9dc976167f45009
Thomas Christinck <thomas.christinck@mail.mcgill.ca>
2017-03-11 17:43:01 -0500

    Add files via upload

---
+++ Operating Systems/Assignment 2/a2_lib.h
@@ -0,0 +1,22 @@
+#ifndef READ_WRITE_H
+#define READ_WRITE_H
+
+//Creates a store if not yet created, and opens the store if created. Returns -1 if failure occurs
+int kv_store_create(char* name);
+
+//Set max size of key and value. Evict the first entry if the store is full. The function should also 
+//update an index maintained in the store
+int kv_store_write(char* key, char* value);
+
+//Takes a key and searches for key-value pair. If found, returns a copy of the value (a pointer to the
+//string is returned, NULL if no pair is found)
+char* kv_store_read(char* key);
+
+//Function should take a key and return all values in the store
+char** kv_store_read_all(char* key);
+
+//Hashing function
+int hash(char* key);
+
+#endif
+//READ_WRITE_H
